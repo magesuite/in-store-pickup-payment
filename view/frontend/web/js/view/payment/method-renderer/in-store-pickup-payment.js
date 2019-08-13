7eaf29e5f48472ae57064d7a8b29219550934a9d
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2019-08-13 14:24:06 +0200

    Adjust frontend implementation to allow placing an order

--- view/frontend/web/js/view/payment/method-renderer/in-store-pickup-payment.js
+++ view/frontend/web/js/view/payment/method-renderer/in-store-pickup-payment.js
@@ -5,14 +5,31 @@
 
 /* @api */
 define([
+    'knockout',
     'Magento_Checkout/js/view/payment/default',
-    'Magento_Checkout/js/model/quote'
-], function (Component, quote) {
+    'Magento_Checkout/js/model/quote',
+], function(ko, Component, quote) {
     'use strict';
 
     return Component.extend({
         defaults: {
-            template: 'MageSuite_InStorePickupPayment/payment/in_store_pickup_payment'
+            template:
+                'MageSuite_InStorePickupPayment/payment/in_store_pickup_payment',
+        },
+        isRadioButtonVisible: ko.observable(true),
+        /**
+         * Always allows placing order with this method as it doesn't require any additional validation.
+         */
+        isPlaceOrderActionAllowed: ko.observable(function() {
+            this.getCode() == this.isChecked();
+        }, this),
+        /**
+         * Copies shipping address to billing address just before order is placed.
+         */
+        placeOrder: function(data, event) {
+            quote.billingAddress(quote.shippingAddress());
+
+            this._super(data, event);
         },
     });
 });
