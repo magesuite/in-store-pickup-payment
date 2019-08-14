3502e169182310525778586e8750ab6822c415d2
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2019-08-14 14:20:04 +0200

    Restore billing address form

--- view/frontend/web/js/view/payment/method-renderer/in-store-pickup-payment.js
+++ view/frontend/web/js/view/payment/method-renderer/in-store-pickup-payment.js
@@ -17,19 +17,5 @@ define([
                 'MageSuite_InStorePickupPayment/payment/in_store_pickup_payment',
         },
         isRadioButtonVisible: ko.observable(true),
-        /**
-         * Always allows placing order with this method as it doesn't require any additional validation.
-         */
-        isPlaceOrderActionAllowed: ko.observable(function() {
-            this.getCode() == this.isChecked();
-        }, this),
-        /**
-         * Copies shipping address to billing address just before order is placed.
-         */
-        placeOrder: function(data, event) {
-            quote.billingAddress(quote.shippingAddress());
-
-            this._super(data, event);
-        },
     });
 });
